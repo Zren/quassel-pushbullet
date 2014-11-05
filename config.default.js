669487f3cdfb46992a3a7f597d0af42b8a256d60
Chris H <zrenfire@gmail.com>
2014-11-05 18:57:22 -0500

    Overhaul the script to list the devices it pushes to, or list them if not configured to.
    Add dependancy on async to simplify callback based code.

--- config.default.js
+++ config.default.js
@@ -15,10 +15,16 @@ var config = {
 				// All Devices: null
 				// Single Device:
 				// 		A DeviceId will look like: ujAjoxHjkmidjAiVsKnSTs
-				// 		To find a deviceId, visit https://www.pushbullet.com/ and click a device.
-				//    The URL should now look like https://www.pushbullet.com/?device_iden=ujAjoxHjkmidjAiVsKnSTs
-				//    Copy the device id from the URL.
+				// 		To find a deviceId, either:
+				//			*	Run quassel-push without a deviceId specified, it will list all devices it pushes to.
+				//				Kill it and set the new id before restarting it.
+				//			*	Visit https://www.pushbullet.com/ and click a device.
+				//				The URL should now look like https://www.pushbullet.com/?device_iden=ujAjoxHjkmidjAiVsKnSTs
+				//				Copy the device id from the URL.
 				deviceId: null,
+				// OR
+				//		Specify a device nickname.
+				deviceNickname: null,
 			}
 		}
 	}
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var Quassel = require('libquassel');
 var PushBullet = require('pushbullet');
+var async = require('async');
 var BufferType = require('libquassel/lib/buffer').IRCBuffer.Types;
 var MessageType = require('libquassel/lib/message').Type;
 
@@ -14,61 +15,115 @@ var logger = {
 	}
 }
 
-function connectToQuasselCore(coreConfig, userConfig) {
-	var pusher = new PushBullet(userConfig.pushbullet.accessToken);	
+function connectToQuasselCore(coreConfig, userConfig, callback) {
+	var pusher = new PushBullet(userConfig.pushbullet.accessToken);
+	var deviceId = userConfig.pushbullet.deviceId;
 
 	function sendNotification(title, body) {
-		logger.debug('[PushBullet]', { deviceId: userConfig.pushbullet.deviceId, title: title, body: body});
-		pusher.note(userConfig.pushbullet.deviceId, title, body, function(err, response) {
+		logger.debug('[PushBullet]', { deviceId: deviceId, title: title, body: body});
+		pusher.note(deviceId, title, body, function(err, response) {
 			if (err) return console.log('[PushBullet] [error]', JSON.stringify(err));
 			logger.debug('[PushBullet]', response);
 		});
 	}
 
-	var quassel = new Quassel(coreConfig.host, coreConfig.port, {
-		nobacklog: true,
-		nodebug: true,
-	}, function(next) {
-		next(userConfig.name, userConfig.pass);
-	});
+	async.series([
+		function(cb) {
+			// Validate deviceId.
+			async.waterfall([
+				function(cb) {
+					pusher.devices(cb);
+				},
+				function(response, cb) {
+					if (userConfig.pushbullet.deviceId || userConfig.pushbullet.deviceNickname) {
+						// Find the device
+						for (var i in response.devices) {
+							var device = response.devices[i];
+							if (deviceId && device.iden == deviceId) {
+								console.log('Pushing to ', device.iden, device.nickname);
+								return cb();
+							} else if (userConfig.pushbullet.deviceNickname && device.nickname == userConfig.pushbullet.deviceNickname) {
+								deviceId = device.iden;
+								console.log('Pushing to ', device.iden, device.nickname);
+								return cb();
+							}
+						}
 
-	// Debugging.
-	quassel.on('**', function() {
-		var args = Array.prototype.slice.call(arguments);
-		args.unshift(this.event);
-		// logger.debug.apply(console, args);
-	});
+						// Device not found.
+						// List devices for user.
+						console.log('Device not found. Please confgire to use one of the following (or none at all).');
+						for (var i in response.devices) {
+							var device = response.devices[i];
+							console.log('', device.iden, device.nickname);
+						}
+						// And exit.
+						return cb({
+							msg: 'Device not found.',
+							exit: true
+						});
 
-	//
-	quassel.on('buffer.message', function(bufferId, messageId) {
-		var buffer = quassel.getNetworks().findBuffer(bufferId);
-		var message = buffer.messages.get(messageId);
+					} else {
+						// deviceId/deviceNickname not specified so we don't need to validate it exists.
+						console.log('Pushing to all devices.');
+						// List devices for user. In case they want to specify only 1 device.
+						for (var i in response.devices) {
+							var device = response.devices[i];
+							console.log('', device.iden, device.nickname);
+						}
+						return cb();
+					}
+				}
+			], cb);
+		},
 
-		if (message.type == MessageType.Plain || message.type == MessageType.Action) {
-			// logger.debug('buffer.message', buffer.name, message.getNick(), message.content);			
+		// Connect to Quassel
+		function(cb) {
+			var quassel = new Quassel(coreConfig.host, coreConfig.port, {
+				nobacklog: true,
+				nodebug: true,
+			}, function(next) {
+				next(userConfig.name, userConfig.pass);
+			});
 
-			if (message.isSelf())
-				return;
+			// Debugging.
+			quassel.on('**', function() {
+				var args = Array.prototype.slice.call(arguments);
+				args.unshift(this.event);
+				// logger.debug.apply(console, args);
+			});
 
-			if (buffer.type == BufferType.QueryBuffer) {
-				var title = message.getNick();
-				var body = message.content;
-				sendNotification(title, body);
-			} else if (buffer.type == BufferType.ChannelBuffer && message.isHighlighted()) {
-				var title = message.getNick() + ' - ' + buffer.name;
-				var body = message.content;
-				sendNotification(title, body);
-			}
+			//
+			quassel.on('buffer.message', function(bufferId, messageId) {
+				var buffer = quassel.getNetworks().findBuffer(bufferId);
+				var message = buffer.messages.get(messageId);
+
+				if (message.type == MessageType.Plain || message.type == MessageType.Action) {
+					// logger.debug('buffer.message', buffer.name, message.getNick(), message.content);			
+
+					if (message.isSelf())
+						return;
+
+					if (buffer.type == BufferType.QueryBuffer) {
+						var title = message.getNick();
+						var body = message.content;
+						sendNotification(title, body);
+					} else if (buffer.type == BufferType.ChannelBuffer && message.isHighlighted()) {
+						var title = message.getNick() + ' - ' + buffer.name;
+						var body = message.content;
+						sendNotification(title, body);
+					}
+				}
+			});
+
+			quassel.connect();
 		}
+	], function(err) {
+		if (err) return console.log('[error]', JSON.stringify(err));
 	});
-
-	quassel.connect();
-
-	return quassel;
 }
 
 function main() {
-	var connection = connectToQuasselCore(config.core, config.core.user);
+	connectToQuasselCore(config.core, config.core.user);
 }
 
 main();
